aef7b2e9bfbbbd92611c930bb47c9205ea6e0766
Camila Moura <camilalnmoura@gmail.com>
2023-05-28 19:50:16 -0300

    Estudando Javascript

---
+++ javaScript1/listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+    {
+        titulo: "JavaScript",
+        preco: 25
+    },
+    {
+        titulo: "PHP",
+        preco: 15
+    }, 
+    {
+        titulo: "JAVA",
+        preco: 30
+    },
+    {
+        titulo: "ELIXER",
+        preco: 50
+    },
+    {
+        titulo: "GO",
+        preco: 45
+    },
+    {
+        titulo: "PYTHON",
+        preco: 20
+    }
+];
+
+module.exports = precosLivros;
